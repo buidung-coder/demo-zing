cd908b8fe157595d752008ba14c36b0144e89984
buidung <buidung314@gmail.com>
2021-09-10 16:47:29 +0700

    commit 1

--- individual.js
+++ individual.js
@@ -1,10 +1,22 @@
 let settingPanel = document.querySelector(".settings-panel")
 let settingBtn = document.querySelector("#setting-icon")
 let exitSetting = document.querySelector(".exit-settings")
+let library = document.querySelector(".library__element")
+let musicSong = document.querySelector(".musicsongs")
+let closeMusicSong = $(".musicsong-back")
+let musicPlayer = $(".music-play")
 
 settingBtn.addEventListener("click", ()=>{
     settingPanel.style.display = "block"
 })
 exitSetting.addEventListener("click", ()=>{
     settingPanel.style.display = "none"
+})
+library.addEventListener("click", ()=>{
+    musicSong.style.display= "block"
+    musicPlayer.style.display= "none"
+})
+closeMusicSong.addEventListener("click", ()=>{
+    musicSong.style.display= "none"
+    musicPlayer.style.display= "block"
 })
